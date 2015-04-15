374c5c76a146b56cde2098dd5d27187034c3bd4d
Jiazhi GAO <sophiakogaojiazhi@gmail.com>
2015-04-15 16:31:47 +0200

    ajouter page_edudiant

---
+++ web/page_edudiant/test.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.fond_menu_poids').slick({
+    slidesToShow: 3, 
+    slidesToScroll: 3,
+    dots: false,
+  	infinite: true,
+  	speed: 300,
+  	adaptiveHeight: true
+  });
+});
