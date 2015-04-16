1c51ce94d611851d6c98675c641bdbf694cd2b3c
Gil 'Ragnar' Felot <gfelot@yandex.com>
2015-04-16 09:52:06 +0200

    OwlCarousel instead of Slick

--- web_refactory/js/balance.js
+++ web_refactory/js/balance.js
@@ -1,21 +1,9 @@
-
-$(document).ready(function() {
-	$('.fond_menu_poids').slick( {
-	  dots: false,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 3,
-	  adaptiveHeight: true,
-	  slidesToScroll: 1
-	} );
-
-
-	$('.fond_menu_elem').slick( {
-	  dots: false,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 3,
-	  adaptiveHeight: true,
-	  slidesToScroll: 1
-	} );
-} );
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+  	margin: 10,
+  	loop: true,
+  	nav: true,
+  	navText: ["Précédant", "Suivant"],
+  	dots: false
+  });
+});
